c36de81cf5b886fe018ee8df23af8e0f4d2ffcdc
Adhika pamungkas <adhikapamungkas22@gmail.com>
2025-07-24 21:21:37 +0700

    update code get articles

--- internal/domain/article/usecase/get_articles.go
+++ internal/domain/article/usecase/get_articles.go
@@ -8,23 +8,23 @@ import (
 
 func (u *ArticleUsecase) GetAll(ctx context.Context, req structs.RequestSearchArticle) (structs.ResponseGetArticle, error) {
 	dest := structs.ResponseGetArticle{}
-	if err := u.cache.Get(ctx, req, &dest); err == nil {
-		return structs.ResponseGetArticle{}, nil
-	}
-	if dest.Total == 0 {
-		respES := structs.ArticleESResponse{}
-		query := req.NewQuerySearchArticle()
-		err := u.esClient.SearchInElasticsearch(ctx, u.conf.ArticleIndex, query, &respES) 
-		if err != nil{
-			return structs.ResponseGetArticle{}, err
+	if dest.Total > 1 {
+		if err := u.cache.Get(ctx, req, &dest); err == nil {
+			return structs.ResponseGetArticle{}, nil
 		}
-		dest = respES.NewResponseGetArticle()
-		dest.Limit = query["size"].(int)
-		dest.Page  = query["from"].(int) / dest.Limit + 1
-		if err := u.cache.Set(ctx, req, &dest); err != nil {
-			return structs.ResponseGetArticle{}, err
-		}
-		return dest, nil
+		return  dest, nil
 	}
-	return  dest, nil
+	respES := structs.ArticleESResponse{}
+	query := req.NewQuerySearchArticle()
+	err := u.esClient.SearchInElasticsearch(ctx, u.conf.ArticleIndex, query, &respES) 
+	if err != nil{
+		return structs.ResponseGetArticle{}, err
+	}
+	dest = respES.NewResponseGetArticle()
+	dest.Limit = query["size"].(int)
+	dest.Page  = query["from"].(int) / dest.Limit + 1
+	if err := u.cache.Set(ctx, req, &dest); err != nil {
+		return structs.ResponseGetArticle{}, err
+	}
+	return dest, nil
 }
